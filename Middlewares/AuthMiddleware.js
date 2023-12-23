d8d300141f07c438690d9c6c8d7d02ce03a04ca0
davit dvalashvili <davitdvalashvili1996@gmail.com>
2023-12-23 12:34:39 +0400

    chore: finish project

--- Middlewares/AuthMiddleware.js
+++ Middlewares/AuthMiddleware.js
@@ -18,7 +18,7 @@ export const userVerification = (req, res) => {
       return res.json({ status: false });
     } else {
       const user = await User.findById(data.id);
-      if (user) return res.json({ status: true, user: user.username });
+      if (user) return res.json({ status: true });
       else return res.json({ status: false });
     }
   });
